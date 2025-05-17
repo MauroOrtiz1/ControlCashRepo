aa7d79ee163121e892663477108a1b3933076a74
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2025-05-17 18:19:41 -0500

    Primer commit del modelo y contexto generado

---
+++ ControlCash/Models/Categorium.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControlCash.Models;
+
+public partial class Categorium
+{
+    public int IdCategoria { get; set; }
+
+    public string NombreCategoria { get; set; } = null!;
+
+    public virtual ICollection<Gasto> Gastos { get; set; } = new List<Gasto>();
+}
---
+++ ControlCash/Models/Exportacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControlCash.Models;
+
+public partial class Exportacion
+{
+    public int IdExportacion { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public string TipoArchivo { get; set; } = null!;
+
+    public string? OrigenGrafico { get; set; }
+
+    public DateTime? FechaExportado { get; set; }
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+}
