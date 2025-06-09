43c0e5cb25390f2fa2bf542c4f7e6653dfee37de
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2025-06-08 21:16:50 -0500

    Primera conexion a la nube - render

--- ControlCash/Models/ControlCashDbContext.cs
+++ ControlCash/Models/ControlCashDbContext.cs
@@ -24,8 +24,10 @@ public partial class ControlCashDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=ControlCashDB;Username=postgres;Password=123456");
+    {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        // => optionsBuilder.UseNpgsql("Host=localhost;Database=ControlCashDB;Username=postgres;Password=123456");
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
